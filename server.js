8d02932effa43b6cbb0904b742a3ed57dcb915f3
Mohit <mohitkr8092@gmail.com>
2021-10-03 22:48:33 +0530

    added express js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = require("express").Router();
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname) + "/index.html");
+});
+
+app.listen(PORT, console.log(`Listening on port ${PORT}.`));
