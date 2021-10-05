0582c4b176c73130874fb910a1c28f558a55067e
Mohit <mohitkr8092@gmail.com>
2021-10-05 22:36:46 +0530

    all

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+require("dotenv").config();
 const express = require("express");
 const router = require("express").Router();
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require("cors");
 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(","),
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+};
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, "")));
 app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname) + "/index.html");
